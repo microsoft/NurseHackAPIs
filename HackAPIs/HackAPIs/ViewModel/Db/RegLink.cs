001e53e32f107e3d746a2a0d42c323eeef0fc425
Daniel Kim <dakim@microsoft.com>
2021-03-19 17:03:07 -0400

    added mock endpoint

---
+++ HackAPIs/HackAPIs/ViewModel/Db/RegLink.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackAPIs.ViewModel.Db
+{
+    public class RegLink
+    {
+        [Key]
+        public int RegLinkId { get; set; }
+        public Guid UniqueCode { get; set; }
+        public string IntendedEmail { get; set; }
+        public string UsedByEmail { get; set; }
+        public string IsUsed { get; set; }
+        public string UserRole { get; set; }
+
+    }
+}
