678dd58a829dd264b0f5b4670bfcd4c724337808
Sameer Doshi <sadoshi@microsoft.com>
2021-09-03 15:18:11 -0500

    Adding key vault population script

---
+++ util/Create-KeyVault.ps1
@@ -0,0 +1,12 @@
+#New-AzKeyVault -VaultName HackKV -ResourceGroupName rg-hackathon-starter-kit -Location 'Central US'
+$kvname="HackKV"
+$secretsimport=Get-Content ./infile.json | ConvertFrom-Json -AsHashtable -Depth 1;
+foreach ($s in $secretsimport.keys){
+    if($s){
+        $val=$secretsimport[$s];
+        $secretvalue = ConvertTo-SecureString $val -AsPlainText -Force
+        Set-AzKeyVaultSecret -VaultName $kvname -Name $s -SecretValue $secretvalue
+        Write-Output $"Added {$s}";
+    }
+
+}
