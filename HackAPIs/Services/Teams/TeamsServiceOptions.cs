02fc89fe05cf621883fa3050b9ca1bb8b41a0706
Chad Voelker <chad.voelker@microsoft.com>
2023-05-11 14:29:31 -0500

    Convert to use list of team ids

--- HackAPIs/Services/Teams/TeamsServiceOptions.cs
+++ HackAPIs/Services/Teams/TeamsServiceOptions.cs
@@ -1,10 +1,11 @@
-﻿namespace HackAPIs.Services.Teams
+﻿using System.Collections.Generic;
+
+namespace HackAPIs.Services.Teams
 {
     public class TeamsServiceOptions
     {
         public string TeamDomain { get; set; }
-        public string MSTeam1 { get; set; }
-        public string MSTeam2 { get; set; }
+        public List<string> TeamIds { get; set; }
         public bool ChannelAutoCreate { get; set; }
     }
 }
